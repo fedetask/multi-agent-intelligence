b82f56e2c756aecb0936fbc9eb6f1ae2388cfc46
Dumitru_Nimara <58092398+ddnimara@users.noreply.github.com>
2020-03-06 20:30:04 +0100

    Added Basic problem 4 functionalities

--- Assets/Scrips/CarAI4.cs
+++ Assets/Scrips/CarAI4.cs
@@ -13,29 +13,38 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public GameObject terrain_manager_game_object;
         TerrainManager terrain_manager;
-
+        public float leader_speed;
         public GameObject[] friends;
         public GameObject[] enemies;
+        public float acceleration_coefficient;
+        private int id; // Identifies the car in the formation
+        public GameObject formation_game_object;
+        
+        private formation formation;
 
         private void Start()
         {
+            
             // get the car controller
             m_Car = GetComponent<CarController>();
             terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
 
-
+            // Get the formtion object
+            formation = formation_game_object.GetComponent<formation>();
             // note that both arrays will have holes when objects are destroyed
             // but for initial planning they should work
             friends = GameObject.FindGameObjectsWithTag("Player");
             enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
-
-
             // Plan your path here
             // ...
         }
 
-
+        public void set_id(int agent_id)
+        {
+            id = agent_id;
+            Debug.Log("Setting my id as "+id);
+        }
         private void FixedUpdate()
         {
 
@@ -43,21 +52,21 @@ namespace UnityStandardAssets.Vehicles.Car
             // Execute your path here
             // ...
 
-            Vector3 avg_pos = Vector3.zero;
+            Vector3 next_pos = formation.get_next_position(id);
 
-            foreach (GameObject friend in friends)
-            {
-                avg_pos += friend.transform.position;
-            }
-            avg_pos = avg_pos / friends.Length;
-            Vector3 direction = (avg_pos - transform.position).normalized;
-
+            Debug.DrawLine(transform.position, next_pos, Color.white, 0.1f);
+            leader_speed = formation.leader_speed;
+            Vector3 direction = (next_pos - transform.position).normalized;
             bool is_to_the_right = Vector3.Dot(direction, transform.right) > 0f;
             bool is_to_the_front = Vector3.Dot(direction, transform.forward) > 0f;
 
             float steering = 0f;
             float acceleration = 0;
+            Vector3 current_direction = transform.forward.normalized;
 
+            float direction_angle = Vector3.Angle(current_direction, direction) * Mathf.Sign(-current_direction.x * direction.z + current_direction.z * direction.x);
+
+            float brake = 0f;
             if (is_to_the_right && is_to_the_front)
             {
                 steering = 1f;
@@ -78,19 +87,29 @@ namespace UnityStandardAssets.Vehicles.Car
                 steering = 1f;
                 acceleration = -1f;
             }
+            
+                acceleration *= Mathf.Clamp(acceleration_coefficient * Vector3.Distance(transform.position, next_pos), 0, 1);
+                //steering = Mathf.Clamp(direction_angle, -m_Car.m_MaximumSteerAngle, m_Car.m_MaximumSteerAngle) * Mathf.Sign(acceleration);
+           
 
-            // this is how you access information about the terrain
-            int i = terrain_manager.myInfo.get_i_index(transform.position.x);
-            int j = terrain_manager.myInfo.get_j_index(transform.position.z);
-            float grid_center_x = terrain_manager.myInfo.get_x_pos(i);
-            float grid_center_z = terrain_manager.myInfo.get_z_pos(j);
+            
+            if(m_Car.CurrentSpeed>30f+ Vector3.Distance(transform.position,next_pos))
+            {
+                brake = 1;
+            }
+           
+            if (Mathf.Abs(steering) < 0.2f)
+            { steering = 0; }
 
-            Debug.DrawLine(transform.position, new Vector3(grid_center_x, 0f, grid_center_z));
+            //if(Vector3.Dot(transform.position- formation.leader_car.transform.position,formation.leader_orientation)>=0)
+            //{
+            //    brake = 1;
+            //}
 
+           
 
             // this is how you control the car
-            Debug.Log("Steering:" + steering + " Acceleration:" + acceleration);
-            m_Car.Move(steering, acceleration, acceleration, 0f);
+            m_Car.Move(steering, acceleration, acceleration, brake);
             //m_Car.Move(0f, -1f, 1f, 0f);
 
 
---
+++ Assets/Scrips/formation.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    public class formation : MonoBehaviour
+    {
+        public GameObject leader_car;
+        public Vector3 leader_orientation;
+        public float leader_speed;
+        public Vector3 previous_position;
+        public int number_of_agents = 4;
+        public float max_length = 40;
+        public List<Vector3> agent_positions;
+
+        bool initialized = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+            leader_orientation = leader_car.transform.forward;
+            previous_position = leader_car.transform.position;
+
+            float[] max_dist = max_distances(leader_orientation, previous_position);
+            agent_positions = line_positions(leader_orientation, previous_position, max_dist);
+
+           
+
+
+        }
+
+
+        public Vector3 get_next_position(int agent_id)
+        {
+            return agent_positions[agent_id];
+        }
+
+        private float[] max_distances(Vector3 leader_orientation, Vector3 leader_position)
+        {
+            Vector3 perpendicular = Vector3.Cross(Vector3.up, leader_orientation); //right orientation
+            RaycastHit hit_right;
+            var mask = ~(1 << LayerMask.NameToLayer("Ingore Raycast")); // Take the mask corresponding to the layer with Name Cube Walls
+            //var mask = ~((1 << LayerMask.NameToLayer("Player")) | (1 << LayerMask.NameToLayer("Ignore Raycast")));
+            Physics.Raycast(leader_position, perpendicular, out hit_right,mask);
+            RaycastHit hit_left;
+            Physics.Raycast(leader_position, -perpendicular, out hit_left,mask);
+            float right = Mathf.Min(max_length, hit_right.distance);
+            float left = Mathf.Min(max_length, hit_left.distance);
+            return new float[2] { left, right };
+        }
+
+        private List<Vector3> line_positions(Vector3 leader_orientation, Vector3 leader_position, float[] distances)
+        {
+            Vector3 perpendicular = Vector3.Cross(Vector3.up, leader_orientation);
+            int number_of_robots_r = number_of_agents / 2; //assume even
+            int number_of_robots_l = number_of_agents - number_of_robots_r;
+            List<Vector3> positions = new List<Vector3>();
+            
+            for (int i = 0; i < number_of_robots_r; i++)
+            {
+                Vector3 position = leader_position - perpendicular.normalized * distances[0] * (number_of_robots_l - i) / number_of_robots_l;
+                positions.Add(position);
+            }
+            for (int i = 0; i < number_of_robots_r; i++)
+            {
+                Vector3 position = leader_position + perpendicular.normalized * distances[1] * (i + 1) / number_of_robots_r;
+                positions.Add(position);
+            }
+
+
+            Debug.DrawLine(positions[0],positions[positions.Count-1],Color.cyan,0.1f);
+            return positions;
+        }
+
+
+
+
+            // Update is called once per frame
+        void Update()
+        {
+            if(!initialized)
+            {
+                GameObject[] friends = GameObject.FindGameObjectsWithTag("Player");
+                int counter = 0;
+                Debug.Log("Friend size " + friends.Length);
+                foreach (GameObject obj in friends)
+                {
+
+                    CarAI4 script = obj.GetComponent<CarAI4>();
+                    Debug.Log("Current object " + obj.name);
+                    script.set_id(counter);
+
+                    counter += 1;
+
+                }
+                initialized = true;
+            }
+            leader_orientation = leader_car.transform.forward;
+            leader_speed = Vector3.Distance(leader_car.transform.position,previous_position) / Time.deltaTime; 
+
+            previous_position = leader_car.transform.position;
+            float[] max_dist = max_distances(leader_orientation, previous_position);
+            agent_positions = line_positions(leader_orientation, previous_position, max_dist);
+
+        }
+        }
+    }
