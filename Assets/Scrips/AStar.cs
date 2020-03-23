c9dca22fbc877980f9e27dfc279e16443b070841
fedetask <32360377+fedetask@users.noreply.github.com>
2020-03-23 22:00:51 +0100

    Problem5 (#2)
    * Added Basic problem 4 functionalities
    
    * Cars are Able to avoid (for the most part) walls
    
    * Follow leader orientation when ahead of him
    
    * Basic Evaluation Matrix Computed
    
    * Astar from player to example enemy
    
    * Added Control (driving) for problem 5
    
    * Switch target at kill
    
    * Played around with some parameters
    
    * Optimize Astar
    
    * Build that gave us a 0 hp win
    
    * Implemented dynamic leader swapping
    
    * 'Prior' added for problem 1
    
    Co-authored-by: Dumitru_Nimara <58092398+ddnimara@users.noreply.github.com>

---
+++ Assets/Scrips/AStar.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    public class AStar : MonoBehaviour
+    {
+
+        public float DISTANCE_COST = 0f;
+        public GameObject terrain_manager_game_object;
+
+        private TerrainManager terrainManager;
+        private TerrainInfo terrainInfo;
+        public GameObject terrain_evaluator_object;
+        private TerrainEvaluator terrain_evaluator;
+        public List<Vector3> path;
+        GameObject[] enemies;
+        GameObject[] friends;
+        // Start is called before the first frame update
+        void Start()
+        {
+            terrainManager = terrain_manager_game_object.GetComponent<TerrainManager>();
+            terrainInfo = terrainManager.myInfo;
+            terrain_evaluator = terrain_evaluator_object.GetComponent<TerrainEvaluator>();
+        }
+
+        public bool initialized = false;
+        private void FixedUpdate()
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                friends = GameObject.FindGameObjectsWithTag("Player");
+            }
+        }
+
+        public float trimPath(List<Vector3> points, GameObject enemy)
+        {
+            float final_cost = 0;
+            Vector3 enemy_location = enemy.transform.position;
+            for (int i=0; i<points.Count; i++)
+            {
+                Vector3 point = points[i];
+                var mask = ~(1 << LayerMask.NameToLayer("Inore Raycast"));
+                bool ray = Physics.Raycast(point, enemy_location, mask);
+                if (!ray)
+                { break; }
+                final_cost += terrain_evaluator.evaluation_matrix[terrainInfo.get_i_index(point.x), terrainInfo.get_j_index(point.z)];
+
+            }
+
+            return final_cost;
+        }
+
+        public (float cost, List<Vector3> points) GetMinimumCostPath(Vector3 from, Vector3 to)
+        {
+            int from_i = terrainInfo.get_i_index(from.x);
+            int from_j = terrainInfo.get_j_index(from.z);
+            int to_i = terrainInfo.get_i_index(to.x);
+            int to_j = terrainInfo.get_j_index(to.z);
+
+
+
+            (float cost, List<Vector2Int> cells) = GetMinimumCostPathCells(new Vector2Int(from_i, from_j),
+                                                                            new Vector2Int(to_i, to_j),
+                                                                            to,
+                                                                            terrain_evaluator.evaluation_matrix);
+                        
+            List<Vector3> path = new List<Vector3>();
+            foreach (Vector2Int cell in cells)
+            {
+                float x = terrainInfo.get_x_pos(cell.x);
+                float z = terrainInfo.get_z_pos(cell.y);
+                path.Add(new Vector3(x, 0, z));
+            }
+            return (cost, path);
+        }
+
+        private float h(Vector2Int from, Vector2Int goal)
+        {
+            return DISTANCE_COST * (Mathf.Abs(goal.x - from.x) + Mathf.Abs(goal.y - from.y));
+        }
+
+        public void update_map(List<GameObject> enemies)
+        {
+            List<Vector3> turret_locations = new List<Vector3>();
+            foreach (GameObject enemy in enemies)
+            {
+                turret_locations.Add(enemy.transform.position);
+            }
+            terrain_evaluator.evaluation_matrix = terrain_evaluator.evaluate_board(terrainInfo.traversability, turret_locations);
+        }
+
+        private (float cost, List<Vector2Int> cells) GetMinimumCostPathCells(Vector2Int from, Vector2Int goal, Vector3 target_pos, float[,] cost_matrix)
+        {
+            int rows = cost_matrix.GetLength(0);
+            int cols = cost_matrix.GetLength(1);
+
+            HashSet<Vector2Int> discovered_nodes = new HashSet<Vector2Int>();
+            Vector2Int[,] came_from = new Vector2Int[rows, cols];
+            float[,] g_scores = new float[rows, cols];
+            float[,] f_scores = new float[rows, cols];
+
+            // Initialization
+            discovered_nodes.Add(from);
+            for (int r = 0; r < g_scores.GetLength(0); r++)
+            {
+                for (int c = 0; c < g_scores.GetLength(1); c++)
+                {
+                    g_scores[r, c] = float.MaxValue;
+                    f_scores[r, c] = float.MaxValue;
+                    came_from[r, c] = new Vector2Int(-1, -1);
+                }
+            }
+            g_scores[from.x, from.y] = 0;
+            f_scores[from.x, from.y] = h(from, goal);
+
+            while (discovered_nodes.Count > 0)
+            {
+                Vector2Int current = ArgMin(discovered_nodes, f_scores);
+                if (current.Equals(goal))
+                {
+                    return (g_scores[goal.x, goal.y], ReconstructPath(came_from, goal));
+                }
+
+                // If from current position we can see the target this is the goal
+                if (see_target(current, target_pos)) {
+                    return (g_scores[current.x, current.y], ReconstructPath(came_from, current));
+                }
+                discovered_nodes.Remove(current);
+
+                foreach (Vector2Int n in GetNeighbors(current, cost_matrix))
+                {
+                    float tentative_score = g_scores[current.x, current.y] + DISTANCE_COST + cost_matrix[n.x, n.y];
+                    if (tentative_score < g_scores[n.x, n.y])
+                    {
+                        came_from[n.x, n.y] = current;
+                        g_scores[n.x, n.y] = tentative_score;
+                        f_scores[n.x, n.y] = g_scores[n.x, n.y] + h(n, goal);
+                        if (!discovered_nodes.Contains(n))
+                        {
+                            discovered_nodes.Add(n);
+                        }
+                    }
+                }
+            }
+
+            Debug.LogError("ERROR NO PATH FOUND");
+            return (-1, null); // Failure
+        }
+
+        private Vector2Int ArgMin(HashSet<Vector2Int> discovered_nodes, float[,] f_scores)
+        {
+            Vector2Int best = new Vector2Int(-1, -1);
+            float min_score = float.MaxValue;
+            foreach (Vector2Int point in discovered_nodes)
+            {
+                if (f_scores[point.x, point.y] < min_score)
+                {
+                    min_score = f_scores[point.x, point.y];
+                    best = point;
+                }
+            }
+            return best;
+        }
+
+        private List<Vector2Int> ReconstructPath(Vector2Int[,] came_from, Vector2Int goal)
+        {
+            List<Vector2Int> res = new List<Vector2Int>();
+            res.Add(goal);
+            Vector2Int current = goal;
+            while (came_from[current.x, current.y].x != -1)
+            {
+                current = came_from[current.x, current.y];
+                res.Insert(0, current);
+            }
+            return res;
+        }
+
+        private List<Vector2Int> GetNeighbors(Vector2Int point, float[,] cost_matrix)
+        {
+            int rows = cost_matrix.GetLength(0);
+            int cols = cost_matrix.GetLength(1);
+            List<Vector2Int> neighbors = new List<Vector2Int>();
+            neighbors.Add(new Vector2Int(point.x, point.y + 1));
+            neighbors.Add(new Vector2Int(point.x + 1, point.y));
+            neighbors.Add(new Vector2Int(point.x - 1, point.y));
+            neighbors.Add(new Vector2Int(point.x, point.y - 1));
+
+            List<Vector2Int> res = new List<Vector2Int>();
+            foreach (Vector2Int n in neighbors)
+            {
+                if (n.x < 0 || n.x >= rows || n.y < 0 || n.y >= cols || cost_matrix[n.x, n.y] > float.MaxValue / 2)
+                {
+                    continue;
+                }
+                res.Add(n);
+            }
+            return res;
+        }
+
+        private bool see_target(Vector2Int current, Vector3 target) {
+            Vector3 current_in_space = new Vector3(terrainInfo.get_x_pos(current.x), 0, terrainInfo.get_z_pos(current.y));
+            Vector3 direction = target - current_in_space;
+            Vector3 normal = new Vector3(-direction.z, direction.y, direction.x).normalized;
+            var mask = ~(1 << LayerMask.NameToLayer("Ignore Raycast"));
+            bool free = ! Physics.Linecast(current_in_space, target, mask);
+
+            if (free) { Debug.DrawLine(current_in_space, target, Color.black, 30f); }
+            return free;
+        }
+
+
+
+    }
+}
---
+++ Assets/Scrips/formation.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    public class formation : MonoBehaviour
+    {
+        public GameObject leader_car;
+        public Vector3 leader_orientation;
+        public float leader_speed;
+        public Vector3 previous_position;
+        public int number_of_agents = 4;
+        public float max_length = 80;
+        public List<Vector3> agent_positions;
+        public bool dynamicLeader = false;
+        public bool initialized = false;
+        AStar aStar;
+        public GameObject aStar_game_object;
+        public GameObject[] friends;
+        private int total_number_of_enemies=8;
+        public int leader_index;
+        // Start is called before the first frame update
+        void Start()
+        {
+            leader_orientation = leader_car.transform.forward;
+            previous_position = leader_car.transform.position;
+
+            float[] max_dist = max_distances(leader_orientation, previous_position);
+            
+            agent_positions = line_positions(leader_orientation, previous_position, max_dist);
+            aStar = aStar_game_object.GetComponent<AStar>();
+        }
+
+
+        public Vector3 get_next_position(int agent_id)
+        {
+            return agent_positions[Mathf.Min(agent_positions.Count-1,agent_id)];
+        }
+
+        private float[] max_distances(Vector3 leader_orientation, Vector3 leader_position)
+        {
+            float margin = 6f;
+            Vector3 perpendicular = Vector3.Cross(Vector3.up, leader_orientation); //right orientation
+            RaycastHit hit_right;
+            var mask = ~(1 << LayerMask.NameToLayer("Inore Raycast")); // Take the mask corresponding to the layer with Name Cube Walls
+            //var mask = ~((1 << LayerMask.NameToLayer("Player")) | (1 << LayerMask.NameToLayer("Ignore Raycast")));
+            Physics.Raycast(leader_position, perpendicular, out hit_right,mask);
+            RaycastHit hit_left;
+            Physics.Raycast(leader_position, -perpendicular, out hit_left,mask);
+            float right = Mathf.Min(max_length, hit_right.distance-margin);
+            float left = Mathf.Min(max_length, hit_left.distance-margin);
+            return new float[2] { left, right };
+        }
+
+       
+        private List<Vector3> line_positions(Vector3 leader_orientation, Vector3 leader_position, float[] distances)
+        {
+            Vector3 perpendicular = Vector3.Cross(Vector3.up, leader_orientation);
+            int number_of_robots_r = number_of_agents / 2; //assume even
+            int number_of_robots_l = number_of_agents - number_of_robots_r;
+            List<Vector3> positions = new List<Vector3>();
+            
+            for (int i = 0; i < number_of_robots_l; i++)
+            {
+                Vector3 position = leader_position - perpendicular.normalized * distances[0] * (number_of_robots_l - i) / number_of_robots_l;
+                positions.Add(position);
+            }
+            for (int i = 0; i < number_of_robots_r; i++)
+            {
+                Vector3 position = leader_position + perpendicular.normalized * distances[1] * (i + 1) / number_of_robots_r;
+                positions.Add(position);
+            }
+
+
+            Debug.DrawLine(positions[0],positions[positions.Count-1],Color.cyan,0.1f);
+            return positions;
+        }
+
+
+
+
+        GameObject current_target = null;
+            // Update is called once per frame
+        void Update()
+        {
+            if(!initialized)
+            {
+                friends = GameObject.FindGameObjectsWithTag("Player");
+                int counter = 0;
+                foreach (GameObject obj in friends)
+                {
+                    CarAI5 script = new CarAI5();
+                    CarAI4 script4 = new CarAI4();
+                    if (!dynamicLeader)
+                    {
+                        script4 = obj.GetComponent<CarAI4>();
+                        script4.set_id(counter);
+                    }
+                    else
+                    {
+                        script = obj.GetComponent<CarAI5>();
+                        script.set_id(counter);
+                    }
+                    
+                   
+                    if (dynamicLeader && counter == 1)
+                    {
+                        script.isLeader = true;
+                    }
+                    if(dynamicLeader && counter==0)
+                    {
+                        script.is_on_left = true;
+                    }
+                    if(dynamicLeader && counter==2)
+                    {
+                        script.is_on_right = true;
+                    }
+                    counter += 1;
+
+                }
+                Debug.Log("finishin initialization");
+                initialized = true;
+            }
+
+
+            if (leader_car == null)
+            {
+                friends = GameObject.FindGameObjectsWithTag("Player");
+
+                CarAI5 script = friends[0].GetComponent<CarAI5>();
+                script.path_counter = leader_index;
+
+                script.isLeader = true;
+                leader_car = friends[0];
+                leader_orientation = leader_car.transform.forward;
+
+            }
+
+            leader_orientation = leader_car.transform.forward;
+            leader_speed = Vector3.Distance(leader_car.transform.position,previous_position) / Time.deltaTime; 
+
+            previous_position = leader_car.transform.position;
+            float[] max_dist = max_distances(leader_orientation, previous_position);
+            agent_positions = line_positions(leader_orientation, previous_position, max_dist);
+            if(dynamicLeader)
+            {
+                List<GameObject> enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
+                // If enemies doesn't contain current_target it means it was killed
+                if (current_target == null || !enemies.Contains(current_target) || enemies.Count < total_number_of_enemies)
+                {
+                    aStar.update_map(enemies);
+                    total_number_of_enemies = enemies.Count;
+                    (current_target, aStar.path) = get_next_target(enemies);
+                }
+
+            }
+
+
+
+
+        }
+
+        (GameObject target, List<Vector3> path) get_next_target(List<GameObject> enemies) {
+            float min_cost = float.MaxValue;
+            GameObject best = null;
+            List<Vector3> path = null;
+            foreach (GameObject enemy in enemies) {
+               
+                (float cost, List<Vector3> points) = aStar.GetMinimumCostPath(leader_car.transform.position, enemy.transform.position);
+                Debug.Log("initial Cost " + cost);
+                cost = aStar.trimPath(points,enemy);
+                Debug.Log("final Cost " + cost);
+                if (cost < min_cost) {
+                    min_cost = cost;
+                    best = enemy;
+                    path = points;
+                }
+            }
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                Debug.DrawLine(path[i], path[i + 1], Color.cyan, 30f);
+            }
+            if(enemies.Count==4)
+            {
+                //friends[0].GetComponent<CarAI5>().id = 2;
+                //friends[2].GetComponent<CarAI5>().id = 0;
+            }
+            friends = GameObject.FindGameObjectsWithTag("Player");
+
+            int counter = 0;
+            foreach (GameObject obj in friends)
+            {
+                CarAI5 script = obj.GetComponent<CarAI5>();
+                script.path_counter = 0;
+                if (script.isLeader)
+                {
+                    script.path=path;
+                }
+            }
+
+            Debug.Log("Best next enemy" + enemies.IndexOf(best));
+                return (best, path);
+        }
+
+        }
+    }
